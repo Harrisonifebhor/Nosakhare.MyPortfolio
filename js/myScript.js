eaa7f9c792a5b1046f5649d163df222458856219
Harrisonifebhor <111140210+Harrisonifebhor@users.noreply.github.com>
2022-09-05 23:36:02 +0100

    initial commit

---
+++ js/myScript.js
@@ -0,0 +1,15 @@
+//typing animation//
+var typed = new Typed(".ty",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","UI/UX Developer","Soon to be Software Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
